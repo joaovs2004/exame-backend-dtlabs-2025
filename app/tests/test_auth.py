3ff11b2aaa4b3109c64479f8dcfac8309c445764
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2025-02-27 13:47:25 +0000

    Add more tests

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -4,8 +4,15 @@ from ..main import app
 
 client = TestClient(app)
 
-def test_create_empty_user():
+def test_create_no_fields_user():
     response = client.post("/auth/register")
+    assert response.status_code == 422
+
+def test_create_empty_user():
+    response = client.post(
+        "/auth/register",
+        json={"name": "", "password": ""}
+    )
     assert response.status_code == 422
 
 def test_create_user_no_password():
@@ -26,5 +33,31 @@ def test_create_user():
     response = client.post(
         "/auth/register",
         json={"name": "teste", "password": "123"}
+    )
+    assert response.status_code == 200
+    assert response.json() == {"message": "User created"}
+
+def test_user_login_no_password():
+    response = client.post(
+        "/auth/login",
+        data={"username": "teste"}
+
+    )
+    assert response.status_code == 422
+
+def test_user_uncreated_login():
+    response = client.post(
+        "/auth/login",
+        data={"username": "usernotcreated", "password": "123"}
+
+    )
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Incorrect username or password"}
+
+def test_user_login():
+    response = client.post(
+        "/auth/login",
+        data={"username": "teste", "password": "123"}
+
     )
     assert response.status_code == 200
